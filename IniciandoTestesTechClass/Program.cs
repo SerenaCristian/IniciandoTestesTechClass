d03cbcbd384e0e3438d9ea862ba8509c54136bd4
SerenaCristian <devcristian02@gmail.com>
2024-11-14 00:06:47 -0300

    Criando o projeto teste

---
+++ IniciandoTestesTechClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IniciandoTestesTechClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
