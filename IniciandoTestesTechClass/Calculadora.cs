44b6251e41ff780a42ed9ca2d68708423ef08fa3
SerenaCristian <devcristian02@gmail.com>
2024-11-14 00:27:45 -0300

    Incluindo Metodos operacionais na classe calculadora

--- IniciandoTestesTechClass/Calculadora.cs
+++ IniciandoTestesTechClass/Calculadora.cs
@@ -6,5 +6,12 @@ namespace IniciandoTestesTechClass
 {
     internal class Calculadora
     {
+        public double Somar(double primeiroNumero, double segundoNumero) => primeiroNumero + segundoNumero; 
+
+        public double Subtracao(double primeiroNumero, double segundoNumero ) => primeiroNumero - segundoNumero;
+
+        public double Multiplicacao(double primeiroNumero, double segundoNumero) => primeiroNumero * segundoNumero;
+
+        public double Divisao(double primeiroNumero, double segundoNumero) => primeiroNumero / segundoNumero;
     }
 }
